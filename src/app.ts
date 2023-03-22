eeeece8afd8e37d8ff9ee2188cc25de95aabeb2f
Estudos <estudos@Abners-MacBook-Pro.local>
2023-03-21 22:46:56 -0300

    Configurando retorno de erros

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
+import { env } from '@/env'
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validationerror', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we shold log to an enternal toll like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -27,8 +27,6 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    // const prismaUsersRepository = new PrismaUsersRepository()
-
     await this.usersRepository.create({
       name,
       email,
